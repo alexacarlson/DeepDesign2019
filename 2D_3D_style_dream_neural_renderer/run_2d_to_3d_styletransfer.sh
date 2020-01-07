c716e7de22a8dae104258c543dc2cd0c7e8b6e8d
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-01-07 14:07:50 -0500

    Update run_2d_to_3d_styletransfer.sh

--- 2D_3D_style_dream_neural_renderer/run_2d_to_3d_styletransfer.sh
+++ 2D_3D_style_dream_neural_renderer/run_2d_to_3d_styletransfer.sh
@@ -3,7 +3,7 @@ INPUT_2D_PATH='/storage/2Dmodels/new000524.png'
 OUTPUT_FILENAME='2Dgeo_3Dtree.gif'
 OUTPUT_DIR='/artifacts/results_2D_to_3D_styletransfer'
  
-python run_examples_style_transfer_3d/run.py \
+python ./run_examples_style_transfer_3d/run.py \
     --filename_mesh ${INPUT_OBJ_PATH} \
     --filename_style ${INPUT_2D_PATH} \
     --filename_output ${OUTPUT_DIR}/${OUTPUT_FILENAME}
