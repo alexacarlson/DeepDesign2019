9d0ab45da0be0890d022b36748380721cf867af1
askc <askc@umich.edu>
2020-01-08 11:30:48 -0500

    updating/testing deep dream 2D code to run in paperspace experiment builder gui

--- paperspace-code-examples/training-pix2pix/README.md
+++
@@ -1,9 +0,0 @@
-# Generating an interactive Pix2Pix model with Gradient° and ml5.js
-
-Companion code for the blog post [Generating an interactive Pix2Pix model with Gradient° and ml5.js]().
-
-This code is based on [github.com/ml5js/training-pix2pix](https://github.com/ml5js/training-pix2pix) with some modifications to work with Gradient°.
-
-## License
-
-MIT
---
+++ run_2Ddeepdream_eval.sh
@@ -0,0 +1,17 @@
+IMAGE_DATA='/storage/2Dmodels/scene0_camloc_0_5_-20_rgb.png'
+WEIGHTS_DIR='/storage/acadia_general_arch_styles_netweights'
+#RESULTS_DIR='/artifacts/test'
+RESULTS_DIR='/storage/test'
+NUM_ITERS=500
+DREAM_CLASS='gothic'
+
+cd 2D_class_based_dreaming
+#visclass-tf \
+python visualize_class.py \
+	--vgg_model ${WEIGHTS_DIR} \
+    --dream_image ${IMAGE_DATA} \
+    --dream_results_dir ${RESULTS_DIR} \
+	--image_h 720 \
+	--image_w 1280 \
+	--dream_class ${DREAM_CLASS} \
+	--iterations ${NUM_ITERS}
