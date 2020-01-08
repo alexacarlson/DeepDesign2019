7fc0f55260c31241515d5c57590f785750ee3b91
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-01-08 16:27:36 -0500

    Update run_2Ddeepdream_eval.sh

--- run_2Ddeepdream_eval.sh
+++ run_2Ddeepdream_eval.sh
@@ -10,12 +10,18 @@ RESULTS_DIR=$2
 NUM_ITERS=$3
 DREAM_CLASS=$4
 
+echo $IMAGE_DATA
+echo $WEIGHTS_DIR
+echo $RESULTS_DIR
+echo $NUM_ITERS
+echo $DREAM_CLASS
+
 cd 2D_class_based_dreaming
 #visclass-tf \
 python visualize_class.py \
 	--vgg_model ${WEIGHTS_DIR} \
-    --dream_image ${IMAGE_DATA} \
-    --dream_results_dir ${RESULTS_DIR} \
+	--dream_image ${IMAGE_DATA} \
+	--dream_results_dir ${RESULTS_DIR} \
 	--image_h 720 \
 	--image_w 1280 \
 	--dream_class ${DREAM_CLASS} \
