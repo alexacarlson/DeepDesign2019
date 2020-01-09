98f9946e8f7c2aa01bd3d6ae8421a2aa98b0b502
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-01-08 21:42:37 -0500

    Update run_2Dto3Dvertexoptimization.sh

--- run_2Dto3Dvertexoptimization.sh
+++ run_2Dto3Dvertexoptimization.sh
@@ -1,8 +1,14 @@
-INPUT_OBJ_PATH='/storage/3Dmodels/TreeCartoon1_OBJ.obj'
-INPUT_2D_PATH='/storage/2Dmodels/new000524.png'
-OUTPUT_FILENAME='2Dgeo_3Dtree'
-OUTPUT_DIR='/artifacts/results_vertoptim'
-NUM_ITERS=250
+#INPUT_OBJ_PATH='/storage/3Dmodels/TreeCartoon1_OBJ.obj'
+#INPUT_2D_PATH='/storage/2Dmodels/new000524.png'
+#OUTPUT_FILENAME='2Dgeo_3Dtree'
+#OUTPUT_DIR='/artifacts/results_vertoptim'
+#NUM_ITERS=250
+
+INPUT_OBJ_PATH=$1
+INPUT_2D_PATH=$2
+OUTPUT_FILENAME=$3
+OUTPUT_DIR=$4
+NUM_ITERS=$5
 
 cd 2D_3D_style_dream_neural_renderer
 
