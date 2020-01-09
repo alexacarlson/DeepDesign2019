af324fabd48a1ffa5dac5fa5c487620ecb9ded17
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-01-08 21:38:30 -0500

    Update run_2Ddeepdream_training.sh

--- run_2Ddeepdream_training.sh
+++ run_2Ddeepdream_training.sh
@@ -1,20 +1,24 @@
-IMAGE_DATA='/storage/scene0_camloc_0_5_-20_rgb.png'
-#IMAGE_DATA='/home/alexandracarlson/Desktop/Robotics_Building_networks/3Dmodels/templeRing/images_rot'
-WEIGHTS_DIR='custom_weights_vgg16_train'
-#RESULTS_DIR='/artifacts/test'
-RESULTS_DIR='/storage/test_train'
-TRAIN_DIR='/mnt/ngv/askc-home/building_motif_dataset/train'
+#IMAGE_DATA='/storage/scene0_camloc_0_5_-20_rgb.png'
+#WEIGHTS_DIR='custom_weights_vgg16_train'
+#RESULTS_DIR='/storage/test_train'
+#TRAIN_DIR='/mnt/ngv/askc-home/building_motif_dataset/train'
+
+TRAIN_DIR=$1
+TRAIN_EPOCHS=$2
+WEIGHTS_DIR=$3
+RESULTS_DIR=$4
+IMAGE_DATA=$5
 
 cd 2D_class_based_dreaming
 
 #visclass-tf \
 python visualize_class.py \
   --train_vgg_model True \
-  --train_epochs 1 \
+  --train_epochs ${TRAIN_EPOCHS} \
   --train_dataset ${TRAIN_DIR} \
-	--vgg_model /storage/${WEIGHTS_DIR} \
+  --vgg_model /storage/${WEIGHTS_DIR} \
   --dream_image ${IMAGE_DATA} \
   --dream_results_dir ${RESULTS_DIR} \
-	--image_h 400 \
-	--image_w 600 \
-	--dream_class 'arch'
+  --image_h 400 \
+  --image_w 600 \
+  --dream_class 'arch'
