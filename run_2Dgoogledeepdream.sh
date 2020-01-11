8ea9eb6808099407e2292592286b0a3c22996646
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-01-11 11:20:21 -0500

    Update run_2Dgoogledeepdream.sh

--- run_2Dgoogledeepdream.sh
+++ run_2Dgoogledeepdream.sh
@@ -1,7 +1,13 @@
 #
-IMAGE_DATA='/storage/2Dmodels/tree1.jpg'
-MODEL_DIR='/storage/inception5_weights'
-NUM_ITERS=30
+#IMAGE_DATA='/storage/2Dmodels/tree1.jpg'
+#MODEL_DIR='/storage/inception5_weights'
+#NUM_ITERS=30
+
+IMAGE_DATA=$1
+MODEL_DIR=$2
+NUM_ITERS=$3
+
+cd google_DeepDreaming/
 
 python deepdream.py \
   --image_file ${IMAGE_DATA} \
