c5390c5401cb71bc24814f1116e95b6d0d334ca5
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-01-11 18:01:49 -0500

    Update run_2Dclassbaseddeepdream_eval.sh

--- run_2Dclassbaseddeepdream_eval.sh
+++ run_2Dclassbaseddeepdream_eval.sh
@@ -14,7 +14,7 @@ IMAGE_W=$7
 
 cd 2D_class_based_dreaming
 #visclass-tf \
-python visualize_class.py \
+python visualize_class_gpu2.py \
 	--vgg_model ${WEIGHTS_DIR} \
 	--dream_image ${IMAGE_DATA} \
 	--dream_results_dir ${RESULTS_DIR} \
@@ -22,3 +22,21 @@ python visualize_class.py \
 	--image_w ${IMAGE_W} \
 	--dream_class ${DREAM_CLASS} \
 	--iterations ${NUM_ITERS}
+	
+#
+#IMAGE_DATA=/storage/Boulder_Grey
+#WEIGHTS_DIR=`pwd`/sam_mandelbrot_classes_netweights
+#DREAMED_CLASS='18PRS11_08_BCN_Pres_MandlbTomography_02'
+#RESULTS_DIR=/home/alexandracarlson/Desktop/boulder_images/dreamedclass${DREAMED_CLASS}_${IMAGE_DATA_NAME}
+#IMAGE_H=900
+#IMAGE_W=900
+#NUM_ITERS=50
+#
+#python /root/visualize_class_gpu2.py \
+#  --vgg_model ${WEIGHTS_DIR} \
+#  --dream_image ${IMAGE_DATA} \
+#  --iterations ${NUM_ITERS} \
+#  --dream_class ${DREAMED_CLASS} \
+#  --dream_results_dir ${RESULTS_DIR} \
+#  --image_h ${IMAGE_H} \
+#  --image_w ${IMAGE_W} \
