31b43d6fbca94e4d3ea815b3cb4ab4e1a25ee2fa
alexacarlson <43515857+alexacarlson@users.noreply.github.com>
2020-01-27 10:10:13 -0500

    Update stylize.py

--- 2D_to_2D_neural_style_transfer/mask-guided-neural-style/stylize.py
+++ 2D_to_2D_neural_style_transfer/mask-guided-neural-style/stylize.py
@@ -1,6 +1,6 @@
 import os
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
+#os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+#os.environ["CUDA_VISIBLE_DEVICES"]="1"
 import sys
 import tensorflow as tf
 import numpy as np
